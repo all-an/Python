f882fe4a756be144f80eed854fd87748f565cd55
all-an <all.ancapistan@gmail.com>
2021-01-29 22:06:01 -0300

    learn

--- english.py
+++ english.py
@@ -1,13 +1,26 @@
+import os
+
+# My daughter also has blue eyes.
+# Janitor , zelador
+# Spills , derramou, entornou
+# Waiter , garçom
+# Plunder , pilhagem
+
 word1 = "Butterscotch"
 sentence1 = "He's rummaging through the cabinets and shelves inside the house."
 
 def part2():
-    attempt2 = input("Ele está vasculhando os armários e prateleiras dentro da casa.")
+    attempt2 = input("Ele está vasculhando os armários e prateleiras dentro da casa. ")
     if attempt2 == sentence1:
         print("Congratulations!")
     else:
         print("Wrong, correct is >>>", sentence1)
-        print("Try again !")
+        os.system('cls||clear')
+        attempt3 = input("Vasculhando ?: ")
+        if attempt3 == "Rummaging":
+            print("Nice !")
+        else:
+            print("Try again !")
         part2() 
 
 def game():
@@ -17,7 +30,7 @@ def game():
         part2()
     else:
         print("Wrong, correct is Butterscotch")
-        print("Try again !")
+        print("Try again")
         game()   
 
 game()
