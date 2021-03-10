e9b4626f2fab9fd80cf00512c5210504b703e14f
all-an <all.ancapistan@gmail.com>
2021-03-10 08:32:42 -0300

    learning

--- 1_0_0/fork.py
+++ 1_0_0/fork.py
@@ -2,6 +2,15 @@
 def play_fork():
     print("----Welcome to the Fork Game----")
 
+    secret_word = "building"
+
+    forked = False
+
+    hit = False
+
+    while not forked and not hit:
+        print("Playing . . .")
+
     print("End of the Game")
 
 if __name__ == "__main__":
