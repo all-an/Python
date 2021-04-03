c44619afc967265385c4b4a8b7b2b51cdd9a0f18
all-an <all.ancapistan@gmail.com>
2021-04-03 13:30:24 -0300

    learning

--- 1_0_0/fork.py
+++ 1_0_0/fork.py
@@ -2,38 +2,24 @@
 def play_fork():
     print("----Welcome to the Fork Game----")
 
-<<<<<<< HEAD
-    secret_word = "banana"
-=======
-    palavra_secreta = "python"
->>>>>>> 6e2048662d7ba52942093dffd4d9eb8b4dbeab68
+    palavra_secreta = "banana"
 
     enforcou = False
-
     acertou = False
 
-<<<<<<< HEAD
-    while not forked and not hit:
-        attempt = input("Type a character: ")
-        print("Playing . . .")
-=======
     while not enforcou and not acertou:
-        chute = input("Qual letra ? ")
 
-        for letra in palavra_secreta:
-            if(chute == letra):
-                print(chute)
-
-        print("jogando . . .")
->>>>>>> 6e2048662d7ba52942093dffd4d9eb8b4dbeab68
+        chute = input("Qual letra? ")
 
         index = 0
-        for character in secret_word:
-            if attempt == character:
-                print("I found the character {} at position {}.".format(character,index))
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print(" Encontrei a letra {} na posição {} ".format(letra, index))
             index = index + 1
 
-    print("End of the Game")
+        print("Jogando . . .")
+
+    print(" Fim ! ")
 
 if __name__ == "__main__":
     play_fork()
