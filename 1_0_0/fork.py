90117078253f061744ebaa73b6cda94da813f3c5
all-an <all.ancapistan@gmail.com>
2021-04-03 13:49:31 -0300

    learning

--- 1_0_0/fork.py
+++ 1_0_0/fork.py
@@ -1,18 +1,14 @@
 def play_fork():
     print("----Welcome to the Fork Game----")
 
-<<<<<<< HEAD
     palavra_secreta = "banana"
-=======
     palavra_secreta = "python"
->>>>>>> fa057bd07eae419333c5ac22d42b7e173045ef6f
 
     enforcou = False
     acertou = False
 
     while not enforcou and not acertou:
 
-<<<<<<< HEAD
         chute = input("Qual letra? ")
 
         index = 0
@@ -20,11 +16,8 @@ def play_fork():
             if(chute == letra):
                 print(" Encontrei a letra {} na posição {} ".format(letra, index))
             index = index + 1
-=======
-    print("jogando . . .")
->>>>>>> fa057bd07eae419333c5ac22d42b7e173045ef6f
 
-        print("Jogando . . .")
+            print("jogando . . .")
 
     print(" Fim ! ")
 
