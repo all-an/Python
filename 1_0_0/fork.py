6c2eb0f9fd31e4160009f44d139046376e037920
Allan Abrahão <all.ancapistan@gmail.com>
2021-04-09 07:25:21 -0300

    learning

--- 1_0_0/fork.py
+++ 1_0_0/fork.py
@@ -1,23 +1,26 @@
 def play_fork():
     print("----Welcome to the Fork Game----")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
     erros = 6
+    fimMensagem = "Vocẽ perdeu !"
+    if acertou:
+        fimMensagem == " Parabéns !"
 
     while not enforcou and not acertou:
 
         index = 0
         chute = input("Qual letra ? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        if(chute.upper() in palavra_secreta.upper()):
+        if(chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
+                if(chute == letra):
                     letras_acertadas[index] = letra
                 index = index + 1
             print("Acertou uma letra.")
@@ -28,8 +31,9 @@ def play_fork():
         # print("Acertou uma letra.")
 
         enforcou = erros == 0
+        acertou = "_" not in letras_acertadas
         print('  '.join(letras_acertadas))
 
-    print(" Fim ! ")
+    print(" Fim do Jogo !", fimMensagem)
 if __name__ == "__main__":
     play_fork()
