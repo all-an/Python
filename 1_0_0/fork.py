e39aea7f1a3cd9c8b2cc3b0bd740773150048cb6
Allan Abrahão <all.ancapistan@gmail.com>
2021-04-15 08:50:43 -0300

    learning

--- 1_0_0/fork.py
+++ 1_0_0/fork.py
@@ -1,6 +1,17 @@
 def play_fork():
     print("----Welcome to the Fork Game----")
 
+    arquivo = open("palavra.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    print(palavras)
+
     palavra_secreta = "signo".upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
 
@@ -8,11 +19,13 @@ def play_fork():
     acertou = False
     erros = 6
     fimMensagem = "Vocẽ perdeu !"
+    advinhe1 = "Adivinhe a palavra: >>  "
+    print(advinhe1, '  '.join(letras_acertadas))
 
     while not enforcou and not acertou:
 
         index = 0
-        chute = input("Qual letra ? ")
+        chute = input("Chute uma letra ? ")
         chute = chute.strip().upper()
 
         if(chute in palavra_secreta):
@@ -21,7 +34,7 @@ def play_fork():
                 if(chute == letra):
                     letras_acertadas[index] = letra
                 index = index + 1
-            print("Acertou uma letra.")
+            print("Acertou uma letra. Lembre-se digite a penas uma letra.")
         else:
             erros -= 1
             print("Errou ! Você tem mais {} tentativas. ".format(erros))
@@ -30,7 +43,7 @@ def play_fork():
 
         enforcou = erros == 0
         acertou = "_" not in letras_acertadas
-        print('  '.join(letras_acertadas))
+        print(advinhe1, '  '.join(letras_acertadas))
 
     if erros > 0:
         fimMensagem = " Parabéns !"
