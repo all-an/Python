13201725575fed4da0d68313ab4bc6a1299117bc
Allan Abrahão <all.ancapistan@gmail.com>
2021-04-16 07:50:17 -0300

    learning

--- 1_0_0/fork.py
+++ 1_0_0/fork.py
@@ -1,6 +1,7 @@
-def play_fork():
-    print("----Welcome to the Fork Game----")
+import random
 
+
+def carrega_palavra():
     arquivo = open("palavra.txt", "r")
     palavras = []
 
@@ -10,14 +11,20 @@ def play_fork():
 
     arquivo.close()
 
-    print(palavras)
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
 
-    palavra_secreta = "signo".upper()
+def play_fork():
+
+    mensagem_abertura()
+
+    carrega_palavra()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
-    erros = 6
+    erros = len(palavras[numero])
     fimMensagem = "Vocẽ perdeu !"
     advinhe1 = "Adivinhe a palavra: >>  "
     print(advinhe1, '  '.join(letras_acertadas))
@@ -50,3 +57,6 @@ def play_fork():
     print(" Fim do Jogo !", fimMensagem)
 if __name__ == "__main__":
     play_fork()
+
+def mensagem_abertura():
+    print("----Welcome to the Fork Game----")
